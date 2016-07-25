6693ea2f12933cae1b714f120c3dd42758d39129
Hirofumi Wakasugi <baenej@gmail.com>
2016-07-25 22:49:05 +0900

    Initial commit

---
+++ spec/language-minitest-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageMinitestView from '../lib/language-minitest-view';
+
+describe('LanguageMinitestView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
